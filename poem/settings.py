5fe08110fd3b9dd9297c497752e9c3510386ce71
Ayatoo047 <101508422+Ayatoo047@users.noreply.github.com>
2022-12-21 23:19:07 +0100

    Yl

--- poem/settings.py
+++ poem/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
